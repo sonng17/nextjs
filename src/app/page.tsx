63a830e00d34ced7a04874554c572b341e262cba
sonng17 <ndson1703@gmail.com>
2024-03-07 11:10:59 +0700

    add css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import x from "@/styles/app.module.css";
+import y from "@/styles/hoidanit.module.css";
 
 export default function Home() {
   return (
     <div>
       <div>Home</div>
-      <Link href="/facebook">facebook</Link>
+      <Link className={x['red']} href="/facebook">facebook</Link>
+      <Link className={y['red']}  href="/youtube">Youtube</Link>
     </div>
   );
 }
