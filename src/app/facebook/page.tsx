be934c5f6b30bfddd68138a786d089b1fb18a305
sonng17 <ndson1703@gmail.com>
2024-03-07 11:24:41 +0700

    add react-bootstrap

--- src/app/facebook/page.tsx
+++ src/app/facebook/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { useRouter } from "next/navigation";
+import Button from "react-bootstrap/Button";
 
 export default function Facebook() {
   const router = useRouter();
   return (
     <div>
       <div>Facebook</div>
+      <Button variant="success">Hoi Dan It</Button>
       <button type="button" onClick={() => router.push("/")}>
         home
       </button>
