a841d062dfb43b5135b9d48f4e9325cbf0a16da5
sonng17 <ndson1703@gmail.com>
2024-03-08 16:58:17 +0700

    add new api

--- src/components/create.modal.tsx
+++ src/components/create.modal.tsx
@@ -4,6 +4,7 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { useState } from "react";
+import { toast } from "react-toastify";
 
 interface IProps {
   showModalCreate: boolean;
@@ -16,7 +17,33 @@ function CreateModal(props: IProps) {
   const [author, setAuthor] = useState<string>("");
   const [content, setContent] = useState<string>("");
   const handleSubmit = () => {
-    console.log(title, author, content);
+    if (!title) {
+      toast.error("Not empty title !");
+      return;
+    }
+    if (!author) {
+      toast.error("Not empty author !");
+      return;
+    }
+    if (!content) {
+      toast.error("Not empty content !");
+      return;
+    }
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, author, content }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res) {
+          toast.success("Create succeed...!");
+          handleCloseModal();
+        }
+      });
   };
   const handleCloseModal = () => {
     setTitle("");
