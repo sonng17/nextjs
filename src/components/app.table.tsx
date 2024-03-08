04530dda4c66870e6973fe937063dc7259762473
sonng17 <ndson1703@gmail.com>
2024-03-08 15:57:39 +0700

    design modal add new

--- src/components/app.table.tsx
+++ src/components/app.table.tsx
@@ -2,6 +2,8 @@
 
 import { Button } from "react-bootstrap";
 import Table from "react-bootstrap/Table";
+import CreateModal from "./create.modal";
+import { useState } from "react";
 
 interface IProps {
   blogs: IBlog[];
@@ -9,34 +11,54 @@ interface IProps {
 
 function AppTable(props: IProps) {
   const { blogs } = props;
-  console.log("check props", blogs);
+
+  const [showModalCreate, setShowModalCreate] = useState<boolean>(false);
+
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>No</th>
-          <th>Title</th>
-          <th>Author</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {blogs?.map((blog) => {
-          return (
-            <tr key={blog.id}>
-              <td>{blog.id}</td>
-              <td>{blog.title}</td>
-              <td>{blog.author}</td>
-              <td>
-                <Button>View</Button>
-                <Button variant="warning" className="mx-3"> Edit</Button>
-                <Button variant="danger">Delete</Button>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </Table>
+    <>
+      <div
+        className="mb-3"
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <h3>Table Blogs</h3>
+        <Button onClick={() => setShowModalCreate(true)} variant="secondary">
+          Add New
+        </Button>
+      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {blogs?.map((blog) => {
+            return (
+              <tr key={blog.id}>
+                <td>{blog.id}</td>
+                <td>{blog.title}</td>
+                <td>{blog.author}</td>
+                <td>
+                  <Button>View</Button>
+                  <Button variant="warning" className="mx-3">
+                    {" "}
+                    Edit
+                  </Button>
+                  <Button variant="danger">Delete</Button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+      <CreateModal
+        showModalCreate={showModalCreate}
+        setShowModalCreate={setShowModalCreate}
+      />
+    </>
   );
 }
 
