d681d97c937ec5e21ca74c0fec534947b10e38a6
sonng17 <ndson1703@gmail.com>
2024-03-09 11:06:02 +0700

    API View detail

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -3,7 +3,9 @@ import AppTable from "@/components/app.table";
 import useSWR from "swr";
 
 const BlogsPage = () => {
+  
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
+ 
   const { data, error, isLoading } = useSWR(
     "http://localhost:8000/blogs",
     fetcher,
